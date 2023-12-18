fe73760ca1eaed3f3a29a26d68c9b828592db6f2
Jose Garcia <garciacolin@hotmail.com>
2023-12-18 17:46:54 -0600

    Building pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import './App.css'
+import './App.css';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Product from './pages/Product.jsx';
+import Pricing from './pages/Pricing.jsx';
+import Login from './pages/Login.jsx';
+import PageNotFound from './pages/PageNotFound.jsx';
 
 function App() {
 
   return (
-    <>
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={ <Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </HashRouter>
   )
 }
 
