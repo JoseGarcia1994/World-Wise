7309812b3adcd5a8a9a8ec0fdc603ebe47e9f20e
Jose Garcia <garciacolin@hotmail.com>
2024-01-10 18:56:23 -0600

    Reading and Setting Query String

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -12,8 +12,11 @@ const formatDate = (date) =>
 const City = () => {
 
   const {id} = useParams()
+  
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
 
-  console.log(x);
   // TEMP DATA
   const currentCity = {
     cityName: "Lisbon",
